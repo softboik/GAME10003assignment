8edd74ab2df36dcbf5da85de27d39501e9131cb4
Olwyn Roberts <000852881@mohawkcollege.ca>
2024-09-13 22:51:52 -0400

    tired

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,10 +24,35 @@ internal class Program
                     if (input == "straight")
                     {
                         Console.WriteLine("You quickly take off into a sprint, Only to be met by a wall. After diving to the side, avoiding the monster that you can hear rumbling the floor behind you, you're met with another choice to make. \nWhich path will you take?");
+
+                        Console.WriteLine("LEFT");
+                        Console.WriteLine("RIGHT");
+
+                        input = Console.ReadLine();
+
+                        if (input == "left")
+                    {
+                        Console.WriteLine("You weave left as you dodge down another corridor. However, you quickly come to realize that this only leads to a dead end! With the monster towering over you, you close your eyes, and- \nGAME OVER");
+                        Console.ReadLine();
+                    }
+                        else if (input == "right")
+                    {
+                        Console.WriteLine("As the Monster comes crashing into the wall behind you, you quickly take off once again to your right. Quickly again you find yourself at a crossroads, how much further will you have to go? \nWhere will you go?");
+
+                        Console.WriteLine("LEFT");
+                        Console.WriteLine("RIGHT");
+
+                        input = Console.ReadLine();
+
+                        if (input == "left")
+                        {
+                            Console.Writeline
+                        }
+                    }
                     }
                     else if (input == "right")
                     {
-                        Console.WriteLine("You dive to your right to avoid a massive fist crashing down into the floor where you stood. As you scramble to your feet you realize there's two paths yet again. \nWhich will you take?");
+                    Console.WriteLine("You dive to your right to avoid a massive fist crashing down into the floor where you stood. As you scramble to your feet you realize there's two paths yet again. \nWhich will you take?");
                     }
                 
             }
