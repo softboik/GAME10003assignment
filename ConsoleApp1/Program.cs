e8869832b96e30e85f05ccde4afb66c4ae6ecb2d
Olwyn Roberts <000852881@mohawkcollege.ca>
2024-09-13 22:17:29 -0400

    fixes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,28 +12,29 @@ internal class Program
 
             input = Console.ReadLine();
             
-            if (input == "PLAY");
+            if (input == "play")
             {
                 Console.WriteLine("You find yourself inside a winding, blocky Maze. Before you have time to get much of a sense of your surroundings, you hear a rather loud roar rumbling within the rocky walls. Seeming muffled at first, it immediately grew louder as a gate opens up behind you. \nWhich way do you run?");
-                {
+                
                     Console.WriteLine("STRAIGHT");
                     Console.WriteLine("RIGHT");
 
                     input = Console.ReadLine();
 
-                    if (input == "STRAIGHT") ;
+                    if (input == "straight")
                     {
                         Console.WriteLine("You quickly take off into a sprint, Only to be met by a wall. After diving to the side, avoiding the monster that you can hear rumbling the floor behind you, you're met with another choice to make. \nWhich path will you take?");
                     }
-                    else if (input == "RIGHT") ;
+                    else if (input == "right")
                     {
-                        Console.WriteLine("You dive to your right to avoid a massive fist crashing down into the floor where you stood. As you scramble to your feet you realize there's two paths yet again. \nWhich will you take?")
+                        Console.WriteLine("You dive to your right to avoid a massive fist crashing down into the floor where you stood. As you scramble to your feet you realize there's two paths yet again. \nWhich will you take?");
                     }
-                }
+                
             }
-            else if (input == "EXIT");
+            else if (input == "exit")
             {
                 Console.WriteLine("Goodbye!");
+                Console.ReadLine();
             }
         }
     }
