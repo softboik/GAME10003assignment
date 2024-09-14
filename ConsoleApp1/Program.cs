8f7de485daa92f6b0d654a3a47d9cbfc1f1e6cbc
Olwyn Roberts <000852881@mohawkcollege.ca>
2024-09-13 21:07:07 -0400

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Player");
+       
+        Console.ReadLine();
+
+        { }
+    }
+}
