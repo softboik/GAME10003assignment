ec63696b8432720337dec3d70d9bf2d5e99c95ae
Olwyn Roberts <000852881@mohawkcollege.ca>
2024-09-13 22:03:10 -0400

    2nd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,36 @@
-﻿internal class Program
+﻿using System.ComponentModel.Design;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello Player");
-       
-        Console.ReadLine();
+        string input;
+        {
+            Console.WriteLine("Hello Player");
+            Console.WriteLine("PLAY");
+            Console.WriteLine("EXIT");
 
-        { }
+            input = Console.ReadLine();
+            
+            if (input == "PLAY");
+            {
+                Console.WriteLine("You find yourself inside a winding, blocky Maze. Before you have time to get much of a sense of your surroundings, you hear a rather loud roar rumbling within the rocky walls. Seeming muffled at first, it immediately grew louder as a gate opens up behind you. \nWhich way do you run?");
+                {
+                    Console.WriteLine("STRAIGHT");
+                    Console.WriteLine("RIGHT");
+
+                    input = Console.ReadLine();
+
+                    if (input == "STRAIGHT") ;
+                    {
+
+                    }
+                }
+            }
+            else if (input == "EXIT");
+            {
+                Console.WriteLine("Goodbye!");
+            }
+        }
     }
 }
